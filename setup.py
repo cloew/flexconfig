8e039ac4a8275a5cdcc0a7d0ed2dede878ac7a2f
Chris Loew <cloew123@gmail.com>
2015-10-30 07:56:32 -0600

    Added Boilerplate Package code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='flexconfig',
+      version='0.0.1',
+      description="",
+      author='',
+      author_email='',
+      packages=['flexconfig'],
+     )
